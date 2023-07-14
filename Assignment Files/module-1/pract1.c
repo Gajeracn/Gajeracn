eeef7068b27f45546f1a4810734086be3dc66d84
Gajeracn <139105327+Gajeracn@users.noreply.github.com>
2023-07-14 12:22:03 +0530

    14-07-2023

---
+++ Assignment Files/module-1/pract1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main()
+{
+    printf("Chirag Gajera\n");
+    printf("My date of birth is 19 september 1889.\n");
+    printf("My age is 36 years.\n");
+    printf("My address is Tulsi Height, Nana Mava Road, Rajkot.");
+
+}
